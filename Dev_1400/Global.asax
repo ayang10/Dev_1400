93fddd680a14cb88d52f3a8239ccba3550e820aa
ayang10 <ayang014@gmail.com>
2017-06-19 20:11:33 -0400

    first commit to new page
    My new personal site is going to be sexy.

---
+++ Dev_1400/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dev_1400.MvcApplication" Language="C#" %>
---
+++ Dev_1400/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dev_1400.Startup))]
+namespace Dev_1400
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
