1a0bb4ca3d68fbe8b7d4a55827333c5ac421dc73
ayang10 <ayang014@gmail.com>
2017-06-20 19:49:37 -0400

    adding sass compiling to page

--- Dev_1400/gulpfile.js
+++ Dev_1400/gulpfile.js
@@ -1,1 +1,8 @@
-﻿
+﻿var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('./assets/sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./assets/css'));
+});
