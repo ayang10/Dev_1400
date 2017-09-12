5b7838dcfcc539f81513baf6a0ba8fef9d40fb63
ayang10 <ayang014@gmail.com>
2017-09-12 16:25:23 -0400

    submit exercise 1 js complete

--- Dev_1400/assets/js/portfoliojs.js
+++ Dev_1400/assets/js/portfoliojs.js
@@ -1,9 +1,54 @@
-﻿$(document).ready(function () {
+﻿// Exercise 1
+document.getElementById("ex1Submit").addEventListener("click", Calculate);
+document.getElementById("ex1Clear").addEventListener("click", Clear);
 
+function Calculate() {
+    var value1 = parseFloat(document.getElementById('number1').value);
+    var value2 = parseFloat(document.getElementById('number2').value);
+    var value3 = parseFloat(document.getElementById('number3').value);
+    var value4 = parseFloat(document.getElementById('number4').value);
+    var value5 = parseFloat(document.getElementById('number5').value);
 
+    if (!isNaN(value1) && !isNaN(value2) && !isNaN(value3) && !isNaN(value4) && !isNaN(value4)) {
+      var lowNum = Math.min(value1, value2, value3, value4, value5);
+      var maxNum = Math.max(value1, value2, value3, value4, value5);
+      var mean = ((value1 + value2 + value3 + value4 + value5) / 5);
+      var sum = parseFloat((value1 + value2 + value3 + value4 + value5));
+      var product = (value1 * value2 * value3 * value4 * value5);
+      var x = document.getElementById("testresult1");
+      x.innerHTML = "<p>The lowest value entered is:  " + lowNum + "</p>";
+      var y = document.getElementById("testresult2");
+      y.innerHTML = "<p>The greatest value entered is: " + maxNum + "</p>";
+      var z = document.getElementById("testresult3");
+      z.innerHTML = "<p>The mean is: " + mean + "</p>";
+      var a = document.getElementById("testresult4");
+      a.innerHTML = "<p>The sum is: " + sum + "</p>";
+      var b = document.getElementById("testresult5");
+      b.innerHTML = "<p>The product is: " + product + "</p>";
+    }
+    else {
+      document.getElementById('errormessage').innerHTML = "<h2><p>Correct your entry!!!!!</p></h2>";
+      document.getElementById('testresult1').innerHTML = "";
+      document.getElementById('testresult2').innerHTML = "";
+      document.getElementById('testresult3').innerHTML = "";
+      document.getElementById('testresult4').innerHTML = "";
+      document.getElementById('testresult5').innerHTML = "";
+    }
+}
 
+function Clear() {
+  document.getElementById('number1').value = 0;
+  document.getElementById('number2').value = 0;
+  document.getElementById('number3').value = 0;
+  document.getElementById('number4').value = 0;
+  document.getElementById('number5').value = 0;
+  document.getElementById('errormessage').innerHTML = "";
+  document.getElementById('testresult1').innerHTML = "";
+  document.getElementById('testresult2').innerHTML = "";
+  document.getElementById('testresult3').innerHTML = "";
+  document.getElementById('testresult4').innerHTML = "";
+  document.getElementById('testresult5').innerHTML = "";
+}
 
+// Exercise 1
 
-
-
-});
