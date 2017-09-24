78e48dd56f93c8c655f7a314ad8bfb1e2a4a81d1
ayang10 <ayang014@gmail.com>
2017-09-23 21:32:03 -0400

    start migration

---
+++ Dev_1400/Models/BlogModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dev_1400.Models
+{
+    public class BlogModels
+    {
+    }
+}
